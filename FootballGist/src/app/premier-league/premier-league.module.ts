97405b4867d08d5a7f18c1ac6f48c11c8b7d5c5d
Akinnusoye Taiwo <taiwoakinnusoye@gmail.com>
2019-10-01 16:42:45 +0100

    initialize premier league module, added angular material library

---
+++ FootballGist/src/app/premier-league/premier-league.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StandingsComponent } from '../standings/standings.component';
+
+
+
+@NgModule({
+  declarations: [StandingsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PremierLeagueModule { }
---
+++ FootballGist/src/config.ts
@@ -0,0 +1,1 @@
+export const api = 'http://api.football-data.org/v2/competitions';
